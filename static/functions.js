04f1cbb37378dc798a1ad7194fd9b01364f04023
Rebecca Mok <61375528+Rebecca-Mok@users.noreply.github.com>
2020-06-13 02:36:01 -0400

    update

--- static/functions.js
+++ static/functions.js
@@ -15,11 +15,74 @@ $(document).ready(function(){
     // $('.col img').css({'height':pw+'px'});
 
     $('.col').click(function(e){
-        // console.log(e.target.id)
-        // if (e.target.id == 1)
-        // $('#HTML').css("display:block;");
-        alert('Clicked ' + e.target.id)
-        console.log(e)
+        if (e.target.id != "")
+            idNum = parseInt(e.target.id)
+        else
+            idNum = parseInt(e.currentTarget.id)
+        
+        // alert(idNum)
+        $('.showProjects').hide();
+        $('.col').css({'background':'rgb(187, 187, 187)', 'color':'black'});
+        
+        if (idNum == 1){
+            $(this).css({'background':'black', 'color':'white'});
+            $('#HTML').toggle();
+
+        }else if (idNum == 2){
+            $(this).css({'background':'black', 'color':'white'});
+            $('#CSS').toggle();
+
+        }else if (idNum == 3){
+            $(this).css({'background':'black', 'color':'white'});
+            $('#JS').toggle();
+
+        }else if (idNum == 4){
+            $(this).css({'background':'black', 'color':'white'});
+            $('#Python').toggle();
+
+        }else if (idNum == 5){
+            $(this).css({'background':'black', 'color':'white'});
+            $('#C').toggle();
+
+        }else if (idNum == 6){
+            $(this).css({'background':'black', 'color':'white'});
+            $('#R').toggle();
+
+        }else if (idNum == 7){
+            $(this).css({'background':'black', 'color':'white'});
+            $('#Verilog').toggle();
+
+        }else if (idNum == 8){
+            $(this).css({'background':'black', 'color':'white'});
+            $('#SQLite').toggle();
+
+        }else if (idNum == 9){
+            $(this).css({'background':'black', 'color':'white'});
+            $('#ArcGIS').toggle();
+
+        }else if (idNum == 10){
+            $(this).css({'background':'black', 'color':'white'});
+            $('#MS').toggle();
+
+        }else if (idNum == 11){
+            $(this).css({'background':'black', 'color':'white'});
+            $('#AutoCAD').toggle();
+
+        }else if (idNum == 12){
+            $(this).css({'background':'black', 'color':'white'});
+            $('#Illustrator').toggle();
+
+        }else if (idNum == 13){
+            $(this).css({'background':'black', 'color':'white'});
+            $('#Photoshop').toggle();
+        }
+
+
+    });
+
+    $('.closeProjects').click(function(e){
+        $('.showProjects').hide();
+        $('.col').css({'background':'rgb(187, 187, 187)', 'color':'black'});
     });
 
 });
